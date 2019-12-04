15f8636ed9a6e0bac33824c235b0dcb39f174ed7
Amirkhan5949 <ak4729176@gmail.com>
2019-12-05 00:34:25 +0530

    done

---
+++ app/src/main/java/com/example/learnfireways/Post.java
@@ -0,0 +1,43 @@
+package com.example.learnfireways;
+
+public class Post {
+   String Title;
+   String author;
+   String discription;
+
+    public Post() {
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "Title='" + Title + '\'' +
+                ", author='" + author + '\'' +
+                ", discription='" + discription + '\'' +
+                '}';
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+}
